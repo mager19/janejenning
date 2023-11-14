9e6ce554c448544212ac41aff6417ecd77c0934e
Mario Germán Reyes <mager19@gmail.com>
2023-11-14 17:13:51 -0500

    initial commit

---
+++ mu-plugins/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ece25cf6f420eff1baf7');
---
+++ mu-plugins/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4d2c9851ceab29b23cd8');
